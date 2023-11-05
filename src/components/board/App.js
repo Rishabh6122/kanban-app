0813ec81a0a930801907c0a1880dcce1b1ebeecc
Rishabh6122 <141906168+Rishabh6122@users.noreply.github.com>
2023-11-05 13:22:44 +0530

    Create App.js

---
+++ src/components/board/App.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState, useCallback } from "react";
+import Board from "./Components/Board/Board";
+import "./App.css";
+import Editable from "./Components/Editabled/Editable";
+
+function App() {
+  const [boards, setBoards] = useState(
+    JSON.parse(localStorage.getItem("prac-kanban")) || []
+  );
+
+  const [targetCard, setTargetCard] = useState({
+    bid: "",
+    cid: "",
+  });
+
+  const addboardHandler = (name) => {
+    const tempBoards = [...boards];
+    tempBoards.push({
+      id: Date.now() + Math.random() * 2,
+      title: name,
+      cards: [],
+    });
+    setBoards(tempBoards);
+  };
+
+  const removeBoard = (id) => {
+    const tempBoards = boards.filter((board) => board.id !== id);
+    setBoards(tempBoards);
+  };
+
+  // ... other existing functions
+
+  const fetchData = useCallback(() => {
+    fetch("https://api.quicksell.co/v1/internal/frontend-assignment")
+      .then((response) => response.json())
+      .then((data) => {
+        setBoards(data.boards || []);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  useEffect(() => {
+    localStorage.setItem("prac-kanban", JSON.stringify(boards));
+  }, [boards]);
+
+  return (
+    <div className="app">
+      <div className="app_nav">
+        <h1>Kanban Board</h1>
+      </div>
+      <div className="app_boards_container">
+        <div className="app_boards">
+          {boards.map((item) => (
+            <Board
+              key={item.id}
+              board={item}
+              addCard={addCardHandler}
+              removeBoard={() => removeBoard(item.id)}
+              removeCard={removeCard}
+              dragEnded={dragEnded}
+              dragEntered={dragEntered}
+              updateCard={updateCard}
+            />
+          ))}
+          <div className="app_boards_last">
+            <Editable
+              displayClass="app_boards_add-board"
+              editClass="app_boards_add-board_edit"
+              placeholder="Enter Board Name"
+              text="Add Board"
+              buttonText="Add Board"
+              onSubmit={addboardHandler}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
